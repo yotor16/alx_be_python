8e165409e840fafc29daad207a051bd680e2ff84
yotor16 <yotorhailu16@gmail.com>
2025-05-25 06:34:11 +0300

    Initial commit: Add basic_operations.py under python_introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10;
+number2 = 5;
+sum = number1 + number2;
+diff = number1 - number2;
+pro = number1 * number2;
+print("Addition of 10 and 5 is : ", sum)
+print("Substraction of 10 and 5 is ", diff)
+print("Multiplication of 10 and 5 is ", pro)
